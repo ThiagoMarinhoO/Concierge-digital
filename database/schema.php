5360c253d0bc4a74eb55321c5c84418b2042bdd6
Thiago Marinho <travailmarinho@gmail.com>
2025-10-30 15:15:02 -0300

    active campaign integration

--- database/schema.php
+++ database/schema.php
@@ -27,3 +27,23 @@ function create_vector_tables() {
     require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
     dbDelta($sql);
 }
+
+function create_active_campaign_tables() {
+    global $wpdb;
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $table_active_campaign_variables = $wpdb->prefix . 'active_campaign_variables';
+
+    $sql = "
+        CREATE TABLE $table_active_campaign_variables (
+            id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            api_url VARCHAR(255) NOT NULL,
+            api_key VARCHAR(255) NOT NULL,
+            assistant_id VARCHAR(255) NOT NULL,
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+        ) $charset_collate;
+    ";
+
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta($sql);
+}
